8ba9d916ff4a47d1fdaf08fd0e35bebf4721858f
dlakmalb <dlakmalb@gmail.com>
2015-05-16 22:21:58 +0530

    2015-05-16 Creat db connection and connected courses forms

--- courses.php
+++ courses.php
@@ -1,160 +1,166 @@
 <?php
 require_once("dbconnection.php");
 require_once("header.php");
-?> 
+?>
+<?php
+mysql_select_db('student_registration_db');
+
+$sql = "SELECT 
+            course_code,
+            course_name,
+            department,
+            `level`,
+            credits
+        FROM courses";
+
+$result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html>
     <?php add_head() ?>
     <body> 
         <div id="wrapper">
-        <?php add_nav() ?>
-        
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Courses Information
-                        </h1>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-offset-3 col-md-2">
-                        <a class="btn btn-info btn-block" href='coursesadd.php'>Add Course</a> <br/>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="panel panel-info">
-                            <div class="panel-heading"> Search Courses </div>
-                            <div class="panel-body">
-                        <form class="form-horizontal" action="" method="GET">
-                            <fieldset>
-                                <div class="form-group">
-                                    <div>
-                                        <label for="labelcode" class="col-md-2 control-label">Course Code</label>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <select class="form-control input-sm" id="selectcode" name="selectcode">
-                                            <option> </option>
-                                            <option>ECX5245</option>
-                                            <option>ECX5267</option>
-                                            <option>ECX5234</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div>
-                                        <label for="labelname" class="col-md-2 control-label">Course Name</label>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <select class="form-control input-sm" id="selectname" name="selectname">
-                                            <option> </option>
-                                            <option>Database Management System</option>
-                                            <option>Software Testing and Quality Assurance</option>
-                                            <option>Data Communication</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div>
-                                        <label for="labeldept" class="col-md-2 control-label">Department</label>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <select class="form-control input-sm" id="selectdept" name="selectdept">
-                                            <option> </option>
-                                            <option>Electrical and Computer Engineering</option>
-                                            <option>Civil Engineering</option>
-                                            <option>Electrical Engineering</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div>
-                                        <label for="labelcredit" class="col-md-2 control-label">Credit</label>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <select class="form-control input-sm" id="selectcredit" name="selectcredit">
-                                            <option> </option>
-                                            <option>0</option>
-                                            <option>3</option>
-                                            <option>6</option>
-                                            <option>9</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div>
-                                        <label for="labellevel" class="col-md-2 control-label">Level</label>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <select class="form-control input-sm" id="selectlevel" name="selectlevel">
-                                            <option> </option>
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                            <option>6</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-2 col-md-offset-2">
-                                        <button type="submit" class="btn btn-primary btn-block">Search</button>
-                                    </div>   
-                                    <div class="col-md-2">
-                                        <a href="courses.php" type="reset" class="btn btn-default btn-block">Clear</a>
+            <?php add_nav() ?>
 
-                                    </div>
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">
+                                Courses Information
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-offset-3 col-md-2">
+                            <a class="btn btn-info btn-block" href='coursesadd.php'>Add Course</a> <br/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-info">
+                                <div class="panel-heading"> Search Courses </div>
+                                <div class="panel-body">
+                                    <form class="form-horizontal" action="" method="GET">
+                                        <fieldset>
+                                            <div class="form-group">
+                                                <div>
+                                                    <label for="selectcode" class="col-md-2 control-label">Course Code</label>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <select class="form-control input-sm" id="selectcode" name="selectcode">
+                                                        <option> </option>
+                                                        <option>ECX5245</option>
+                                                        <option>ECX5267</option>
+                                                        <option>ECX5234</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div>
+                                                    <label for="selectname" class="col-md-2 control-label">Course Name</label>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <select class="form-control input-sm" id="selectname" name="selectname">
+                                                        <option> </option>
+                                                        <option>Database Management System</option>
+                                                        <option>Software Testing and Quality Assurance</option>
+                                                        <option>Data Communication</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div>
+                                                    <label for="selectdept" class="col-md-2 control-label">Department</label>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <select class="form-control input-sm" id="selectdept" name="selectdept">
+                                                        <option> </option>
+                                                        <option>Electrical and Computer Engineering</option>
+                                                        <option>Civil Engineering</option>
+                                                        <option>Electrical Engineering</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div>
+                                                    <label for="selectcredit" class="col-md-2 control-label">Credit</label>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <select class="form-control input-sm" id="selectcredit" name="selectcredit">
+                                                        <option> </option>
+                                                        <option>0</option>
+                                                        <option>3</option>
+                                                        <option>6</option>
+                                                        <option>9</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div>
+                                                    <label for="selectlevel" class="col-md-2 control-label">Level</label>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <select class="form-control input-sm" id="selectlevel" name="selectlevel">
+                                                        <option> </option>
+                                                        <option>1</option>
+                                                        <option>2</option>
+                                                        <option>3</option>
+                                                        <option>4</option>
+                                                        <option>5</option>
+                                                        <option>6</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-md-2 col-md-offset-2">
+                                                    <button type="submit" class="btn btn-primary btn-block">Search</button>
+                                                </div>   
+                                                <div class="col-md-2">
+                                                    <a href="courses.php" type="reset" class="btn btn-default btn-block">Clear</a>
+
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </form>
                                 </div>
-                            </fieldset>
-                        </form>
                             </div>
                         </div>
                     </div>
-                </div>
-                <table id="table" class="table table-hover table-bordered">
-                    <thead>	
-                        <tr>
-                            <th>Course Code</th>
-                            <th>Course Name</th>
-                            <th>Department</th>
-                            <th>Level</th>
-                            <th>Credits</th>
-                            <th>Course Fee</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>ECX5245</td>
-                            <td>Database Management System</td>
-                            <td>Electrical and Computer Engineering</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>6600.00</td>
-                            <td> <a href='updatecomplaint.php?complain_id=" . $row['complain_id'] . "'>edit</a></td>
-                            <td> <a href='updatecomplaint.php?complain_id=" . $row['complain_id'] . "'>delete</a></td>
-                        </tr>
-                        <tr>
-                            <td>ECX5267</td>
-                            <td>Software Testing and Quality Assurance</td>
-                            <td>Electrical and Computer Engineering</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>6600.00</td>
-                            <td> <a href='updatecomplaint.php?complain_id=" . $row['complain_id'] . "'>edit</a></td>
-                            <td> <a href='updatecomplaint.php?complain_id=" . $row['complain_id'] . "'>delete</a></td>
-                        </tr>
-                    </tbody>
+                    <table id="table" class="table table-hover table-bordered">
+                        <thead>	
+                            <tr>
+                                <th>Course Code</th>
+                                <th>Course Name</th>
+                                <th>Department</th>
+                                <th>Level</th>
+                                <th>Credits</th>
+                                <th>Course Fee</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
 
+                        <?php
+                        while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+                            echo "<tr>";
 
+                            echo "<td>" . $row['course_code'] . "</td>";
+                            echo "<td>" . $row['course_name'] . "</td>";
+                            echo "<td>" . $row['department'] . "</td>";
+                            echo "<td>" . $row['level'] . "</td>";
+                            echo "<td>" . $row['credits'] . "</td>";
+                            echo "<td>" . $row['credits'] . "</td>";
+                            echo "<td> <a href=''>edit</a></td>";
+                            echo "<td> <a href=''>delete</a></td>";
 
-                </table>
-            </div> 
-        </div>
+                            echo "</tr>";
+                        }
+                        ?>
+
+                    </table>
+                </div> 
+            </div>
         </div>
         <script>$("#selectcode").select2({placeholder: "Select Course Code", allowClear: true});</script>
         <script>$("#selectname").select2({placeholder: "Select Course Name", allowClear: true});</script>
--- coursesadd.php
+++ coursesadd.php
@@ -2,6 +2,29 @@
 require_once("dbconnection.php");
 require_once("header.php");
 ?>
+<?php
+// get user inputs
+if (isset($_POST['register'])):
+    $code = ($_POST['inputcoursecode']);
+    $name = ($_POST['inputcoursename']);
+    $department = ($_POST['inputdepartment']);
+    $level = ($_POST['inputlevel']);
+    $credits = ($_POST['inputcredits']);
+
+    $sql = "INSERT INTO courses (course_code, course_name, credits, department, level)
+            VALUES('" . $code . "', '" . $name . "',  '" . $credits . "', '" . $department . "', '" . $level . "')";
+
+    $result = $conn->query($sql);
+    if ($result && $conn->affected_rows > 0) {
+        header("Location: courses.php");
+        exit;
+    } else {
+        echo '<script language = "javascript">';
+        echo 'alert("Course Already Exist")';
+        echo '</script>';
+    }
+endif;
+?>
 <!DOCTYPE html>
 <html>
     <?php add_head() ?>
@@ -22,38 +45,38 @@ require_once("header.php");
                             <form class="form-horizontal" action="" method="POST">
                                 <fieldset>
                                     <div class="form-group">
-                                        <label for="labelcoursecode" class="col-lg-3 control-label">Course Code</label>
+                                        <label for="inputcoursecode" class="col-lg-3 control-label">Course Code</label>
                                         <div class="col-lg-9">
                                             <input required='required' class="form-control input-sm textBorder" id="inputcoursecode" name="inputcoursecode" placeholder="Course Code (Ex. ECX5245)" type="text">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="labelcoursename" class="col-lg-3 control-label">Course Name</label>
+                                        <label for="inputcoursename" class="col-lg-3 control-label">Course Name</label>
                                         <div class="col-lg-9">
                                             <input required='required' class="form-control input-sm textBorder" id="inputcoursename" name="inputcoursename" placeholder="Course Name" type="text">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="labeldepartment" class="col-lg-3 control-label">Department</label>
+                                        <label for="inputdepartment" class="col-lg-3 control-label">Department</label>
                                         <div class="col-lg-9">
                                             <input required='required' class="form-control input-sm textBorder" id="inputdepartment" name="inputdepartment" placeholder="Department" type="text">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="labellevel" class="col-lg-3 control-label">Level</label>
+                                        <label for="inputlevel" class="col-lg-3 control-label">Level</label>
                                         <div class="col-lg-9">
                                             <input required='required' class="form-control input-sm textBorder" id="inputlevel" name="inputlevel" placeholder="Level" type="text">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="labelcredits" class="col-lg-3 control-label">Credits</label>
+                                        <label for="inputcredits" class="col-lg-3 control-label">Credits</label>
                                         <div class="col-lg-9">
                                             <input required='required' class="form-control input-sm textBorder" id="inputcredits" name="inputcredits" placeholder="Credits" type="text">
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="col-lg-3 col-lg-offset-6">
-                                            <button type="submit" class="btn btn-primary btn-block">Register</button>
+                                            <button onclick="duplicate_id()" type="submit" name= "register" class="btn btn-primary btn-block">Register</button>
                                         </div>
                                         <div class="col-lg-3">
                                             <button type="reset" class="btn btn-default btn-block">Clear</button>
